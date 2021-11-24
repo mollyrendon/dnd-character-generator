419914538e6da45b9537131268ad803ac9328767
Hilliard Stamper <hilliard.stamper@att.net>
2021-11-24 15:44:52 -0500

    added var to capture object names in array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let modalBtn = document.getElementById("save-name")
 let modal = document.querySelector(".character-modal")
 let closeBtn = document.querySelector(".close")
+// let characters = [data.results[0].name, data.results[1].name, data.results[2].name]
+
 
 modalBtn.onclick = function(){
     modal.style.display = "block"
@@ -22,20 +24,26 @@ var getAlignments = function() {
     fetch(alignmentsUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-            console.log("response ok");
-            console.log(data);
+                console.log(data.results[0].name); 
+                let characters = [data.results[0].name, data.results[1].name, data.results[2].name, data.results[3].name, data.results[4].name, data.results[5].name, data.results[6].name, data.results[7].name, data.results[6].name]
+                console.log(characters); 
+                console.log(data.results); 
+                
             })
         }
     });
-};
+
+}              
+            
+    
+
 
 var getClasses = function() {
     var classesUrl = "https://www.dnd5eapi.co/api/classes";
     fetch(classesUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                console.log("response ok");
-                console.log(data);
+                
             })
         }
     });
@@ -46,12 +54,11 @@ var getRaces = function() {
     fetch(racesUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                console.log("response ok");
-                console.log(data);
+                
             })
         }
-    });
-}
+    })
+};
     
 
 getAlignments();
