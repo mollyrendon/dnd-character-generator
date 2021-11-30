578589e758f600fe3361059e30f7cf46af3aee76
kelsey-schenk <kelsey.schenk@att.net>
2021-11-29 20:39:13 -0500

    remove console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,6 @@ var getAlignments = function() {
         if (response.ok) {
             response.json().then(function(data){
                 let alignment = [data.results[0].name, data.results[1].name, data.results[2].name, data.results[3].name, data.results[4].name, data.results[5].name, data.results[6].name, data.results[7].name, data.results[8].name]
-                console.log(alignment); 
 
 
 
@@ -40,8 +39,6 @@ var getAlignments = function() {
                                     let allDice = data.dice[0].value -1
                                     characterAlignment = alignment[allDice]; 
                                     resultAlignment.innerText = characterAlignment;
-                                    console.log(allDice);
-                                    console.log(characterAlignment);
                                 return characterAlignment;
                                 }) 
                             }
@@ -66,7 +63,6 @@ var getClasses = function() {
         if (response.ok) {
             response.json().then(function(data){
                 let classes = [data.results[0].name, data.results[1].name, data.results[2].name, data.results[3].name, data.results[4].name, data.results[5].name, data.results[6].name, data.results[7].name, data.results[8].name, data.results[9].name, data.results[10].name, data.results[11].name]
-                console.log(classes);
 
 
                 btnClass.addEventListener("click", function() {
@@ -78,8 +74,6 @@ var getClasses = function() {
                                     let allDice = data.dice[0].value -1
                                     characterClass = classes[allDice]; 
                                     resultClass.innerText = characterClass;
-                                    console.log(allDice);
-                                    console.log(characterClass);
                                 return;
                                 }) 
                             }
@@ -100,7 +94,6 @@ var getRaces = function() {
         if (response.ok) {
             response.json().then(function(data){
                 let race = [data.results[0].name, data.results[1].name, data.results[2].name, data.results[3].name, data.results[4].name, data.results[5].name, data.results[6].name, data.results[7].name, data.results[8].name]
-                console.log(race);
 
                 btnRace.addEventListener("click", function() {
                     var getRollAll = function() {
@@ -111,8 +104,6 @@ var getRaces = function() {
                                     let allDice = data.dice[0].value - 1
                                     characterRace = race[allDice]; 
                                     resultRace.innerText = characterRace;
-                                    console.log(allDice);
-                                    console.log(characterRace);
                                 return;
                                 }) 
                             }
@@ -150,22 +141,18 @@ window.onclick = function(event) {
 }
 
 
-
+// Local Storage
 document.getElementById("save-btn").addEventListener("click", function() {
     var characterName = document.getElementById("characternameinput").value;
-    console.log(JSON.stringify(characterName));
     localStorage.setItem("characternameinput",JSON.stringify(characterName));
 
     var characterClass1 = document.getElementById("class-input").value;
-    console.log(JSON.stringify(characterClass1));
     localStorage.setItem("class-input",JSON.stringify(characterClass1));
 
     var characterRace1 = document.getElementById("race-input").value;
-    console.log(JSON.stringify(characterRace1));
     localStorage.setItem("race-input",JSON.stringify(characterRace1));
 
     var characterAlignment1 = document.getElementById("align-input").value;
-    console.log(JSON.stringify(characterAlignment1));
     localStorage.setItem("align-input",JSON.stringify(characterAlignment1));
 })
 
@@ -193,9 +180,3 @@ getAlignments();
 getClasses();
 getRaces();
 displayCharacter();
-
-
-
-//Randomizer
-
-//Local Storage
